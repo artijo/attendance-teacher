b07e7352a6117a39856ba7339ff7ec8a9dad6e3a
ApichaiTi <81065469+artijo@users.noreply.github.com>
2025-03-17 15:30:12 +0700

    Add logout functionality to clear tokens and redirect to login

--- src/App.jsx
+++ src/App.jsx
@@ -67,6 +67,12 @@ function App() {
     document.querySelector("header").classList.toggle("active");
   }
 
+  const Logout = async () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    window.location.href = "/login";
+  }
+
   async function refreshTokens() {
     const refreshToken = localStorage.getItem("refreshToken");
     try {
@@ -231,7 +237,7 @@ function App() {
         </nav>
         <div className="hidden mt-10 ml-4 md:flex items-center">
         <svg viewBox="0 0 24 24" width={25} fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path fillRule="evenodd" clipRule="evenodd" d="M21.593 10.943c.584.585.584 1.53 0 2.116L18.71 15.95c-.39.39-1.03.39-1.42 0a.996.996 0 0 1 0-1.41 9.552 9.552 0 0 1 1.689-1.345l.387-.242-.207-.206a10 10 0 0 1-2.24.254H8.998a1 1 0 1 1 0-2h7.921a10 10 0 0 1 2.24.254l.207-.206-.386-.241a9.562 9.562 0 0 1-1.69-1.348.996.996 0 0 1 0-1.41c.39-.39 1.03-.39 1.42 0l2.883 2.893zM14 16a1 1 0 0 0-1 1v1.5a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1.505a1 1 0 1 0 2 0V5.5A2.5 2.5 0 0 0 12.5 3h-7A2.5 2.5 0 0 0 3 5.5v13A2.5 2.5 0 0 0 5.5 21h7a2.5 2.5 0 0 0 2.5-2.5V17a1 1 0 0 0-1-1z" fill="#ffffff"></path></g></svg>
-          <button className="p-2 md:p-3">ออกจากระบบ</button>
+          <button className="p-2 md:p-3" onClick={Logout}>ออกจากระบบ</button>
         </div>
       </header>
       <main className="md:ml-56 min-h-dvh">
